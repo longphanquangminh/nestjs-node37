9a6fac76e6b953d52c67b1cb97d565efd2927b04
longphanquangminh <lordaizensousuke2001@gmail.com>
2023-12-17 09:49:06 +0700

    up

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,37 @@
-import { Controller } from '@nestjs/common';
+import {
+  BadRequestException,
+  Controller,
+  HttpCode,
+  HttpException,
+  HttpStatus,
+  Post,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+  @HttpCode(201)
+  @Post('login')
+  login() {
+    try {
+      // sai mật khẩu hoặc email
+      // throw new HttpException('Sai email', 400);
+      // throw new HttpException('Sai email', HttpStatus.BAD_REQUEST);
+      throw new BadRequestException('Sai email');
+
+      return this.authService.login();
+    } catch (exception) {
+      if (exception.status !== 500) {
+        throw new HttpException(exception.response, exception.status);
+      }
+      throw new HttpException('Lỗi ...', HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+  }
+
+  @Post('signup')
+  signup() {
+    return this.authService.signup();
+  }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,14 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  login() {
+    return 'token';
+  }
+
+  signup() {
+    return 'Đăng ký thành công';
+  }
+}
+
+// yarn add @nestjs/passport passport passport-local @nestjs/jwt passport-jwt @types/passport-jwt
